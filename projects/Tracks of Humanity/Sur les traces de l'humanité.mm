06c16dc81152303605dbdcae97babb8f84bf5022
PYM <pym.aldebaran@gmail.com>
2013-02-09 16:00:43 +0100

    Creation du projet Tracks Of Humanity

---
+++ projects/Tracks of Humanity/Sur les traces de l'humanité.mm
@@ -0,0 +1,7 @@
+<map version="0.9.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Sur les traces de l&apos;humanit&#xe9;" ID="ID_1085441654" CREATED="1358274827587" MODIFIED="1358274876075">
+<hook NAME="MapStyle" max_node_width="600"/>
+<node TEXT="" POSITION="right" ID="ID_1100361822" CREATED="1358274919367" MODIFIED="1358274919367"/>
+</node>
+</map>
